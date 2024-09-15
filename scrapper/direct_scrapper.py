82dcd422d294cf17d52eec3db79d57942b90587a
Matheus Jurgensen <mjurgensen@usp.br>
2024-09-15 19:07:01 -0300

    Selects correct stations and allows retrieving data from multiple years.

--- scrapper/direct_scrapper.py
+++ scrapper/direct_scrapper.py
@@ -1,6 +1,7 @@
 import requests
 from meta_data import stations, indicators
 from credentials import login, password
+import os
 
 
 def get_session_id():
@@ -23,7 +24,7 @@ def get_session_id():
     return session_id
 
 
-def get_data(session_id, interval_years=(2000, 2024)):
+def get_data(session_id, interval_years=(2000, 2024), interval_size=1):
     url = "https://qualar.cetesb.sp.gov.br/qualar/exportaDadosAvanc.do?method=exportar"
 
     cookie_value = f"JSESSIONID={session_id}"
@@ -33,9 +34,9 @@ def get_data(session_id, interval_years=(2000, 2024)):
 
     for station in stations:
         for indicator in indicators:
-            for year in range(interval_years[1], interval_years[0], -1):
+            for year in range(interval_years[1], interval_years[0], -interval_size):
                 payload = {
-                    'dataInicialStr': f"01/01/{year - 1}",
+                    'dataInicialStr': f"01/01/{year - interval_size}",
                     'dataFinalStr': f"01/01/{year}",
                     'estacaoVO.nestcaMonto': stations[station],
                     'nparmtsSelecionados': indicators[indicator],
@@ -43,13 +44,17 @@ def get_data(session_id, interval_years=(2000, 2024)):
 
                 response = requests.post(url, headers=headers, data=payload)
                 if not ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" in response.text):
-                    save_csv_file(response.text, station, indicator, str(year - 1), str(year))
+                    save_csv_file(response.text, station, indicator, str(year - interval_size), str(year))
+                    print(f"Succesfully saved: {station} - {indicator} - {year - interval_size}:{year}")
 
 
 def save_csv_file(data, station_name, indicator_name, start_year, end_year):
-    fp = open(f"./scrapped_data/{station_name}_{indicator_name}_{start_year}_{end_year}.csv", "w")
-    fp.write(data)
-    fp.close()
+    directory = "./scrapped_data"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = f"{directory}/{station_name}_{indicator_name}_{start_year}_{end_year}.csv"
+    with open(file_path, "w") as fp:
+        fp.write(data)
 
 
-get_data(get_session_id())
+get_data(get_session_id(), interval_size=8)
