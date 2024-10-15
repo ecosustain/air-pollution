753f34152d1f594d186d20d35512c8aac7b052fc
Matheus Jurgensen <mjurgensen@usp.br>
2024-10-15 15:58:42 -0300

    Also update csvs when updating the database

--- scrapper/direct_scrapper.py
+++ scrapper/direct_scrapper.py
@@ -1,6 +1,5 @@
 '''
 TO-DO:
-- Update csv files.
 - Refactor scripts (create file for update).
 '''
 
@@ -32,24 +31,39 @@ def get_session_id():
         session_id = cookie.value
     return session_id
 
-def get_data(session_id, interval_years=(2000, 2024), interval_size=1):
-    url = "https://qualar.cetesb.sp.gov.br/qualar/exportaDadosAvanc.do?method=exportar"
-    headers = {
-        'Cookie': f"JSESSIONID={session_id}"
-    }
+def check_valid_dates(start_date, end_date):
+    try:
+        start_date_obj = datetime.strptime(start_date, "%d/%m/%Y")
+        end_date_obj = datetime.strptime(end_date, "%d/%m/%Y")
+        return end_date_obj > start_date_obj
+    except ValueError:
+        return False
 
+def get_request_response(session_id, start_date, end_date, station, indicator):
+    if not check_valid_dates(start_date, end_date):
+        return None
+    url = "https://qualar.cetesb.sp.gov.br/qualar/exportaDadosAvanc.do?method=exportar"
+    headers = {'Cookie': f"JSESSIONID={session_id}"}
+    payload = { 'dataInicialStr': start_date,
+                'dataFinalStr': end_date,
+                'estacaoVO.nestcaMonto': station,
+                'nparmtsSelecionados': indicator    }
+    
+    response = requests.post(url, headers=headers, data=payload)
+    if ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" in response.text):
+        return None
+    return response.text
+
+def get_data(session_id, interval_years=(2000, 2024), interval_size=1):
     for station in stations:
         for indicator in indicators:
             for year in range(interval_years[1], interval_years[0], -interval_size):
-                payload = {
-                    'dataInicialStr': f"01/01/{year - interval_size}",
-                    'dataFinalStr': f"01/01/{year}",
-                    'estacaoVO.nestcaMonto': stations[station][0],
-                    'nparmtsSelecionados': indicators[indicator],
-                }
-                response = requests.post(url, headers=headers, data=payload)
-                if not ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" in response.text):
-                    save_csv_file(response.text, station, indicator, str(year - interval_size), str(year))
+                start_date = f"01/01/{year - interval_size}"
+                end_date = f"01/01/{year}"
+                response_text = get_request_response(session_id, start_date, end_date,
+                                                     stations[station][0], indicators[indicator])
+                if response_text is not None:
+                    save_csv_file(response_text, station, indicator, str(year - interval_size), str(year))
                     print(f"Succesfully saved: {station} - {indicator} - {year - interval_size}:{year}")
 
 def get_df_from_csv(directory, file_name):
@@ -66,41 +80,44 @@ def get_df_from_csv(directory, file_name):
 
 def get_dates_to_update(df):
     df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')
-    max_datetime = df['datetime'].max()
-    count_max_date = (df['datetime'] == max_datetime).sum()
+    max_date = df['datetime'].dt.date.max()
+    count_max_date = (df['datetime'].dt.date == max_date).sum()
     if count_max_date > 1:
-        start_date = (max_datetime + pd.Timedelta(days=1)).strftime("%d/%m/%Y")
+        start_date = (max_date + pd.Timedelta(days=1)).strftime("%d/%m/%Y")
     else:
-        start_date = max_datetime.strftime("%d/%m/%Y")
+        start_date = max_date.strftime("%d/%m/%Y")
     end_date = (datetime.now().date() - timedelta(days=1)).strftime("%d/%m/%Y")
     return start_date, end_date
 
-def update_data(session_id):
-    url = "https://qualar.cetesb.sp.gov.br/qualar/exportaDadosAvanc.do?method=exportar"
-    headers = {
-        'Cookie': f"JSESSIONID={session_id}"
-    }
-
-    data_directory = "./data/scraped_data"
+def update_data(session_id, data_directory="./data/scraped_data"):
     for file in os.listdir(data_directory):
+        dfs_to_update_csv = {}
         df = get_df_from_csv(data_directory, file)
         if df is None:
             continue
-
         start_date, end_date = get_dates_to_update(df)
         station = file[:-4]
         for indicator in indicators:
-            payload = {
-                'dataInicialStr': start_date,
-                'dataFinalStr': end_date,
-                'estacaoVO.nestcaMonto': stations[station][0],
-                'nparmtsSelecionados': indicators[indicator],
-            }
-            response = requests.post(url, headers=headers, data=payload)
-            if ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" in response.text):
-                continue
-            update_database(response.text, station, indicator)
-            print(f"Succesfully updated: {station} - {indicator} - up to {end_date}")
+            response_text = get_request_response(session_id, start_date, end_date,
+                                                     stations[station][0], indicators[indicator])
+            if response_text is not None:
+                df_to_update_csv = update_database(response_text, station, indicator)
+                print(f"Successful database update: {station} - {indicator} - up to {end_date}")
+                dfs_to_update_csv[indicator.lower()] = df_to_update_csv
+        update_csv_file(df, dfs_to_update_csv, data_directory, file)
+        
+def update_csv_file(original_df, dfs_to_update_csv, directory, file_name):
+    for indicator, new_data in dfs_to_update_csv.items():
+        original_df = pd.merge(original_df, new_data, on='datetime', how='outer', suffixes=('', '_new'))
+        if f"{indicator}_new" in original_df.columns:
+            original_df[indicator] = original_df[indicator].fillna(original_df[f"{indicator}_new"])
+            original_df.drop(columns=[f"{indicator}_new"], inplace=True)
+    csv_file_path = os.path.join(directory, file_name)
+    try:
+        original_df.to_csv(csv_file_path, index=False)
+        print(f"Succesfully updated {file_name}.")
+    except:
+        print(f"Failure while updating {file_name}.")
 
 def fix_time_format(date_str):
     if "24:00" in date_str:
@@ -130,7 +147,10 @@ def adjust_columns_and_data(df, station, indicator):
     df.drop_duplicates(inplace=True)
     df = df[df['value'].notnull()]
     df['value'] = df['value'].astype(float)
-    return df.dropna()
+
+    df_to_update_csv = df[['datetime', 'value']].copy() # will be used for updating the csv file
+    df_to_update_csv.rename(columns={'value': indicator.lower()}, inplace=True)
+    return df.dropna(), df_to_update_csv.dropna()
 
 def update_database(data, station, indicator):
     db_connection = create_engine('mysql+pymysql://root:root@localhost/poluicao')
@@ -138,14 +158,17 @@ def update_database(data, station, indicator):
         file_path = os.path.join(temp_dir, "temp_file.csv")
         with open(file_path, "w") as file:
             file.write(data)
-            update_measure_indicator_table(file_path, station, indicator, db_connection)
+            df_to_update_csv = update_measure_indicator_table(file_path, station, indicator,
+                                                              db_connection)
             update_station_indicators_table(station, indicator, db_connection)
+    return df_to_update_csv
             
 def update_measure_indicator_table(file_path, station, indicator, db_connection):
     df = pd.read_csv(file_path, sep=";", skiprows=7, encoding="latin1").dropna()
-    df = adjust_columns_and_data(df, station, indicator)
+    df, df_to_update_csv = adjust_columns_and_data(df, station, indicator)
     df.to_sql('measure_indicator', con=db_connection, if_exists='append',
                 index=False, chunksize=1000)
+    return df_to_update_csv
 
 def update_station_indicators_table(station, indicator, db_connection):
     id_station = stations[station][0]
