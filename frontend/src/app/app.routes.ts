fae9b3f1cee147c2a0b29dc60da643159e747dc3
Kaiqoliver <kakadeoliveira@outlook.com>
2024-10-16 21:45:40 -0300

    Started frontend, started the creation of the heatmapform component

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HeatmapFormComponent } from './heatmap-form/heatmap-form.component';
+
+export const routes: Routes = [
+    { path: 'heatmap-form', component: HeatmapFormComponent }
+];
