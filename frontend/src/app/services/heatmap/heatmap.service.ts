a5ec1aeb1d65a93718350e6188734b3c1358449a
Kaiqoliver <kakadeoliveira@outlook.com>
2024-10-20 17:35:24 -0300

    Introduced HttpClient on heatmap service

--- frontend/src/app/services/heatmap/heatmap.service.ts
+++ frontend/src/app/services/heatmap/heatmap.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Point } from '../../models/point.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeatmapService {
 
-  constructor() { }
+  private url = environment.api
+
+  constructor(private httpClient: HttpClient) { }
+
+  getInterpolatedPoints(){
+    return this.httpClient.get<Point[]>(this.url + '/heat_map')
+  }
 }
