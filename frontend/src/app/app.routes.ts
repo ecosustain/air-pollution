cca86025b5c39320a67c32eb79154defec93d1f3
Kaiqoliver <kakadeoliveira@outlook.com>
2024-10-20 15:59:17 -0300

    Generated graph component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HeatmapFormComponent } from './heatmap-form/heatmap-form.component';
 import { HeatmapComponent } from './heatmap/heatmap.component';
-
+import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
     { path: 'heatmap-form', component: HeatmapFormComponent },
-    { path: 'heatmap', component : HeatmapComponent }
+    { path: 'heatmap', component : HeatmapComponent },
+    { path: 'home', component : HomeComponent }
 ];
--- frontend/src/app/heatmap/heatmap.component.ts
+++ frontend/src/app/heatmap/heatmap.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeatmapFormComponent } from "../heatmap-form/heatmap-form.component";
 
 @Component({
   selector: 'app-heatmap',
   standalone: true,
-  imports: [],
+  imports: [HeatmapFormComponent],
   templateUrl: './heatmap.component.html',
   styleUrl: './heatmap.component.css'
 })
