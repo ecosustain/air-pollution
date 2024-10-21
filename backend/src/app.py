706b6a0ee441d5a84fb5d25d8091976b1329304a
Kaiqoliver <kakadeoliveira@outlook.com>
2024-10-20 23:31:32 -0300

    Fixing app.py - backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,12 +4,13 @@ from flask import (
     make_response,
     jsonify,
 )
-
+from flask_cors import CORS  # Import flask_cors
 from controllers import (
     HeatMapController,
     UpdateController,
 )
 
+import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys, os
@@ -24,10 +25,12 @@ Session = sessionmaker(bind=engine)
 SESSION = Session()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return "<h1>MAC 0476<h1/>"
+
 
 @app.route('/update_data', methods=['PUT'])
 def update_data():
@@ -35,26 +38,29 @@ def update_data():
 
     if request.method == 'PUT':
         response = UpdateController().update_data()
-        
+
         response = jsonify(response)
         response.status = 200
-    
+
     return response
-    
-@app.route('/heat_map', methods=['GET'])
-def heat_map():
-    payload = request.get_json()
+
+
+@app.route('/heat_map/<string:payload>', methods=['GET'])
+def heat_map(payload):
+    print(payload)
+    payload = json.loads(payload)
+    print(payload)
     response = make_response()
 
     if request.method == 'GET':
         heat_map = HeatMapController(session=SESSION).get_heat_map(payload=payload)
         SESSION.close()
-        
+
         response = jsonify({"heat_map": heat_map})
         response.status = 200
 
     return response
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',
-            debug=True)
+    app.run(host='0.0.0.0', debug=True)
