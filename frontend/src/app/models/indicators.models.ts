fa3b2900b2c3e7e26e0c18333afa6878fe32ce8e
Kaiqoliver <kakadeoliveira@outlook.com>
2024-11-14 17:15:45 -0300

    Indicators and interpolation methods informations

--- frontend/src/app/models/indicators.models.ts
+++ frontend/src/app/models/indicators.models.ts
@@ -1,26 +1,8 @@
-export const indicators = {
-    "MP2.5" : {
-        interval : [0, 25, 50, 75, 125],
-        measureUnit : "µg/m³" 
-    },
-    "MP10" : {
-        interval : [0, 50, 100, 150, 250],
-        measureUnit : "µg/m³" 
-    },
-    "O3" : {
-        interval : [0, 100, 130, 160, 200],
-        measureUnit : "µg/m³" 
-    },
-    "CO" : {
-        interval : [0, 9, 11, 13, 15],
-        measureUnit : "ppm" 
-    },
-    "NO2" : {
-        interval : [0, 200, 240, 320, 1130],
-        measureUnit : "µg/m³" 
-    },
-    "SO2" : {
-        interval : [0, 20, 40, 365, 800],
-        measureUnit : "µg/m³" 
-    }
-}
+export const indicators = [
+    {name : "MP2.5", interval : [0, 25, 50, 75, 125], measureUnit : "µg/m³"},
+    {name : "MP10", interval : [0, 50, 100, 150, 250], measureUnit : "µg/m³"},
+    {name : "O3", interval: [0, 100, 130, 160, 200], measureUnit : "µg/m³"},
+    {name : "CO", interval : [0, 9, 11, 13, 15], measureUnit : "ppm"},
+    {name : "NO2", interval : [0, 200, 240, 320, 1130], measureUnit : "µg/m³"},
+    {name : "SO2", interval : [0, 20, 40, 365, 800], measureUnit : "µg/m³"}
+]
