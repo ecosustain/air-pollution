896a0017b662e3bf0a7d2c241d5643e9dc6fc87f
Kaiqoliver <kakadeoliveira@outlook.com>
2024-11-14 17:15:45 -0300

    Now the heatmap form changes its form controls according to the time interval selected by user.

--- frontend/src/app/components/heatmap-form/heatmap-form.component.ts
+++ frontend/src/app/components/heatmap-form/heatmap-form.component.ts
@@ -1,81 +1,95 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-heatmap-form',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './heatmap-form.component.html',
-  styleUrl: './heatmap-form.component.css'
+  styleUrls: ['./heatmap-form.component.css']
 })
 export class HeatmapFormComponent implements OnInit {
   mapaDeCalorForm: FormGroup;
-  timePeriodType: string = '';
+  intervalType: string = '';
+  methodType: string = '';
   @Output() formSubmit = new EventEmitter<any>();
 
   constructor(private fb: FormBuilder) {
     this.mapaDeCalorForm = this.fb.group({
       indicator: ['', Validators.required],
-      method:['', Validators.required],
-      param:['', Validators.required], 
-      timePeriod: ['', Validators.required], 
-      startYear: [''],    
-      endYear: [''],     
-      year: [''],        
-      specificDate: this.fb.group({   
-        day: [''],
-        month: [''],
-        year: [''],
-        hour : ['']
+      interpolator : this.fb.group({
+        method: ['', Validators.required],
+        params: this.fb.group({})
       }),
+      interval: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {}
 
-  onTimePeriodChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    const selectedValue = selectElement.value;
-    this.timePeriodType = selectedValue;
-    this.clearValidators();
-  
-    if (this.timePeriodType === 'Anual') {
-      this.mapaDeCalorForm.get('startYear')?.setValidators([Validators.required]);
-      this.mapaDeCalorForm.get('endYear')?.setValidators([Validators.required]);
-    } else if (this.timePeriodType === 'Mensal') {
-      this.mapaDeCalorForm.get('year')?.setValidators([Validators.required]);
-    } else if (this.timePeriodType === 'Diária' || this.timePeriodType === 'Horária' ) {
-      this.mapaDeCalorForm.get('specificDate.day')?.setValidators([Validators.required]);
-      this.mapaDeCalorForm.get('specificDate.month')?.setValidators([Validators.required]);
-      this.mapaDeCalorForm.get('specificDate.year')?.setValidators([Validators.required]);
-      this.mapaDeCalorForm.get('specificDate.hour')?.setValidators([Validators.required]);
+  onIntervalChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.intervalType = selectedValue;
+    this.clearTimeDynamicControls();
+
+    if (this.intervalType === 'yearly') {
+      this.mapaDeCalorForm.addControl('startYear', this.fb.control('', Validators.required));
+      this.mapaDeCalorForm.addControl('endYear', this.fb.control('', Validators.required));
+    } else if (this.intervalType === 'monthly') {
+      this.mapaDeCalorForm.addControl('year', this.fb.control('', Validators.required));
+    } else if (this.intervalType === 'daily' || this.intervalType === 'hourly') {
+      this.mapaDeCalorForm.addControl('specificDate', this.fb.group({
+        day: ['', Validators.required],
+        month: ['', Validators.required],
+        year: ['', Validators.required],
+        hour: this.intervalType === 'hourly' ? this.fb.control('', Validators.required) : this.fb.control('')
+      }));
     }
     this.mapaDeCalorForm.updateValueAndValidity();
   }
 
-  clearValidators() {
-    this.mapaDeCalorForm.get('startYear')?.clearValidators();
-    this.mapaDeCalorForm.get('endYear')?.clearValidators();
-    this.mapaDeCalorForm.get('year')?.clearValidators();
-    this.mapaDeCalorForm.get('specificDate.day')?.clearValidators();
-    this.mapaDeCalorForm.get('specificDate.month')?.clearValidators();
-    this.mapaDeCalorForm.get('specificDate.year')?.clearValidators();
-    this.mapaDeCalorForm.get('specificDate.hour')?.clearValidators();
+  onMethodChange(event: Event) {
+    this.methodType = (event.target as HTMLSelectElement).value;
+    const params = this.mapaDeCalorForm.get('interpolator')?.get('params') as FormGroup;
+    this.clearParamsDynamicControls();
 
-    this.mapaDeCalorForm.get('startYear')?.reset();
-    this.mapaDeCalorForm.get('endYear')?.reset();
-    this.mapaDeCalorForm.get('year')?.reset();
-    this.mapaDeCalorForm.get('specificDate')?.reset();
+    if (this.methodType === 'KNN') {
+      params.addControl('k', this.fb.control('', Validators.required));
+    } else if (this.methodType === 'Krigin') {
+      params.addControl('method', this.fb.control('', Validators.required));
+      params.addControl('variogram_model', this.fb.control('', Validators.required));
+      params.addControl('n_lags', this.fb.control('', Validators.required));
+      params.addControl('weight', this.fb.control(false, Validators.required));
+    }
+    params.updateValueAndValidity();
   }
+
+  clearTimeDynamicControls() {
+    ['startYear', 'endYear', 'year', 'specificDate'].forEach(control => {
+      this.mapaDeCalorForm.removeControl(control);
+    });
+  }
+
+  clearParamsDynamicControls() {
+    const params = this.mapaDeCalorForm.get('interpolator.params') as FormGroup;
+    ['k', 'method', 'variogram_model', 'n_lags', 'weight'].forEach(control => {
+      if (params.get(control)) {
+        params.removeControl(control);
+      }
+    });
+    params.reset();
+    params.clearValidators();
+  }
+  
 
   onSubmit() {
     if (this.mapaDeCalorForm.valid) {
-      console.log('Form submitted:', this.mapaDeCalorForm.value);
-      this.formSubmit.emit(this.mapaDeCalorForm.value);
+      const formValue = this.mapaDeCalorForm.value;
+      console.log(formValue)
+      this.formSubmit.emit(formValue);
     } else {
-      console.log('Form is invalid');
+      console.log('Form is invalid:', this.mapaDeCalorForm.value);
     }
   }
 }
