a4d3d5726f2e3a052bf8fe9a2f63db9cba5cdec3
Kaiqoliver <kakadeoliveira@outlook.com>
2024-11-14 16:34:45 -0300

    In heatmap component legend changes according to the indicator. In heatmap component colors change according to the indicator. Adapted to the new way of receiving the points for the heatmaps.

--- frontend/src/app/services/heatmap/heatmap.service.ts
+++ frontend/src/app/services/heatmap/heatmap.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Heatmap } from '../../models/point.model';
+import { HeatmapResponse } from '../../models/point.model';
 import { environment } from '../../../environments/environment';
 
 
@@ -18,24 +18,15 @@ export class HeatmapService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getInterpolatedPoints(
-    date: string,
-    indicator: string,
-    method: string,
-    params: any
-  ): Observable<Heatmap> {
-    let payload = {
-      datetime : date,
-      indicator : indicator,
-      interpolator : {
-        method : method,
-        params : params
-      } 
-    }
+  // Method to get interpolated points
+  getInterpolatedHeatmap(
+    payload : object
+  ): Observable<HeatmapResponse> {
     
     let payload_encoded = JSON.stringify(payload)
     
-    return this.httpClient.get<Heatmap>(this.url+'/'+ payload_encoded, 
+    // Use get method with the constructed URL and query parameters
+    return this.httpClient.get<HeatmapResponse>(this.url+'/'+ payload_encoded, 
       {
         headers : this.headers
       }
