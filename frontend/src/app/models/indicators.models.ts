219da8b9dfe9c43ae42b34f2888fa7434e1963bb
Kaiqoliver <kakadeoliveira@outlook.com>
2024-11-25 20:04:34 -0300

    Quick Fix: quality intervals for temp and ur not in right format

--- frontend/src/app/models/indicators.models.ts
+++ frontend/src/app/models/indicators.models.ts
@@ -5,6 +5,6 @@ export const indicators = [
     {name : "CO", qualityIntervals : [9, 11, 13, 15], qualityColors : ['green', 'yellow', 'pink', 'red', 'purple'], measureUnit : "ppm"},
     {name : "NO2", qualityIntervals : [200, 240, 320, 1130], qualityColors : ['green', 'yellow', 'pink', 'red', 'purple'], measureUnit : "µg/m³"},
     {name : "SO2", qualityIntervals : [20, 40, 365, 800], qualityColors : ['green', 'yellow', 'pink', 'red', 'purple'], measureUnit : "µg/m³"},
-    {name : "TEMP", qualityIntervals : [0, 15, 20, 25, 30, 35, 40], qualityColors : ['blue', 'cyan', 'green', 'yellow', 'orange', 'red', 'darkred'], measureUnit : "°C"},
-    {name : "UR", qualityIntervals : [0, 20, 40, 60, 80, 100], qualityColors : ['darkred', 'red', 'yellow', 'green', 'cyan', 'blue'], measureUnit : "%"}
+    {name : "TEMP", qualityIntervals : [15, 20, 25, 30, 35, 40], qualityColors : ['blue', 'cyan', 'green', 'yellow', 'orange', 'red', 'darkred'], measureUnit : "°C"},
+    {name : "UR", qualityIntervals : [20, 40, 60, 80, 100], qualityColors : ['darkred', 'red', 'yellow', 'green', 'cyan', 'blue'], measureUnit : "%"}
 ]
