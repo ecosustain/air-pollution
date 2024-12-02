20a5419aef57a3acced23526cd4a5e6445c76d22
Tobias Rossi Müller <48110428+NewbieeSaibot@users.noreply.github.com>
2024-12-02 18:53:46 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { HeatmapComponent } from './components/heatmap/heatmap.component';
 import { HomeComponent } from './components/home/home.component';
 import { GraphComponent } from './components/graph/graph.component';
 import { GraphFormComponent } from './components/graph-form/graph-form.component';
+
 export const routes: Routes = [
-    { path: 'heatmap-form', component: HeatmapFormComponent },
-    { path: 'heatmap', component : HeatmapComponent },
-    { path: '', component : HomeComponent },
-    { path: 'linegraph-form', component: GraphFormComponent},
-    { path: 'linegraph', component: GraphComponent}
+  { path: 'heatmap-form', component: HeatmapFormComponent },
+  { path: 'heatmap', component: HeatmapComponent },
+  { path: 'air-pollution', component: HomeComponent }, // Rota para HomeComponent
+  { path: '', redirectTo: '/air-pollution', pathMatch: 'full' }, // Redireciona a rota raiz para /air-pollution
+  { path: 'linegraph-form', component: GraphFormComponent },
+  { path: 'linegraph', component: GraphComponent },
+  { path: '**', redirectTo: '/air-pollution' } // Redireciona rotas inválidas para HomeComponent
 ];
